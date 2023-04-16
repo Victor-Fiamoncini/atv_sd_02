f919f363f2a25f556caa39348dcb75fc48e299a6
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2023-04-16 16:55:22 -0300

    feat: add ConvertPostsToJsonService contract

---
+++ server/convert_posts_to_json_service.py
@@ -0,0 +1,11 @@
+''' This module defines the ConvertPostsToJsonService abstract class '''
+
+from unimplemented_method_exception import UnimplementedMethodException
+
+class ConvertPostsToJsonService:
+    ''' ConvertPostsToJsonService class '''
+
+    def execute(self, *args) -> None:
+        ''' Execute the conversion of posts to json '''
+
+        raise UnimplementedMethodException()
