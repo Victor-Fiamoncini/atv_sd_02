cc47453ddcbcf9d9aa85139d05b24b6598589e6c
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2023-04-16 16:55:57 -0300

    feat: add base RpcConvertPostsToJsonService impl

---
+++ server/rpc_convert_posts_to_json_service.py
@@ -0,0 +1,42 @@
+''' This module defines the RpcConvertPostsToJsonService class '''
+
+import rpyc
+
+from convert_posts_to_json_service import ConvertPostsToJsonService
+from mappers import map_post_dict_to_post_dto
+from post_to_txt_thread_executor import PostToTxtThreadExecutor
+
+@rpyc.service
+class RpcConvertPostsToJsonService(ConvertPostsToJsonService):
+    ''' RpcConvertPostsToJsonService class '''
+
+    post_to_txt_thread_executor: PostToTxtThreadExecutor = None
+
+    def __init__(self, post_to_txt_thread_executor: PostToTxtThreadExecutor) -> None:
+        super().__init__()
+
+        self.post_to_txt_thread_executor = post_to_txt_thread_executor
+
+    @property
+    def post_to_txt_thread_executor(self):
+        ''' PostToTxtThreadExecutor getter '''
+
+        return self._post_to_txt_thread_executor
+
+    @post_to_txt_thread_executor.setter
+    def post_to_txt_thread_executor(self, value):
+        ''' PostToTxtThreadExecutor setter '''
+
+        self._post_to_txt_thread_executor = value
+
+    @rpyc.exposed
+    def execute(self, *args) -> None:
+        ''' Call convert posts to json concurrent routines '''
+
+        posts_json = args[0][0]
+        mapped_posts = map(map_post_dict_to_post_dto, posts_json)
+
+        self.post_to_txt_thread_executor.posts = list(mapped_posts)
+
+        for generated_filename in self.post_to_txt_thread_executor.execute_concurrently():
+            print(f'File generated: {generated_filename}')
---
+++ server/unimplemented_method_exception.py
@@ -0,0 +1,4 @@
+''' This module defines the UnimplementedMethodException class '''
+
+class UnimplementedMethodException(Exception):
+    ''' Raised when a method has no implementation '''
